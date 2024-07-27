2e6bbf1ad055d37d2ddb0981193c0aaabe7c0b4e
m-nt-s-no <45675504+m-nt-s-no@users.noreply.github.com>
2024-07-27 16:14:23 +0000

    added instance variables to app.rb, added square views

--- app.rb
+++ app.rb
@@ -2,8 +2,35 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:layout) do
+    #nested erb idea from: https://levelup.gitconnected.com/yield-in-yield-in-ruby-erb-for-sinatra-web-app-37d445363afe
+    #but leads to double header :(
+    erb(:square_new)
+  end
+end
+
+get("/square/new") do
+  @input = params.fetch("user_number").to_i
+  @square = @input ** 2
+
+end
+
+get("/square_root/new") do
+  @input = params.fetch("user_number").to_i
+  @square = @input ** (1/2)
+end
+
+get("/payment/new") do
+  @apr = (params.fetch("user_apr").to_f / 100) / 12
+  @loan_term = params.fetch("user_years").to_i * 12
+  @principal = params.fetch("user_principal").to_f
+  @numerator = @apr * @principal
+  @denominator = 1 - ((1 + @apr) ** -@loan_term)
+  @payment = (@numerator/@denominator).to_fs(:currency, { :precision => 2 })
+end
+
+get("/random/new") do
+  @min = params.fetch("user_min").to_i
+  @max = params.fetch("user_max").to_i
+  @rand_num = rand(@min..@max) 
 end
